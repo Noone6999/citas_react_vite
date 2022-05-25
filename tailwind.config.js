510139c1cea427276e89ac168c988d89462c6bf5
juan <jm.franco99@hotmail.com>
2022-05-25 21:18:19 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["index.html", "./src/**/*.jsx"], //Elimina todo lo que no se esta ussando
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+      extend: {},
+  },
+  variants: {
+      extend: {},
+  },
+  plugins: [],
+};
